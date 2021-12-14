b9aecc2a62e2a7edb150039771386c0816088432
Maarten Trompper <maartentrompper@freedom.nl>
2021-12-14 19:46:43 +0100

    First commit

--- Sources/MyKeyboardKitPreview/MyKeyboardKitPreview.swift
+++ Sources/MyKeyboardKitPreview/MyKeyboardKitPreview.swift
@@ -1,6 +1,39 @@
-public struct MyKeyboardKitPreview {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import KeyboardKit
 
-    public init() {
-    }
+private class FakeMockDevice: UIDevice {
+    override var userInterfaceIdiom: UIUserInterfaceIdiom { .phone }
+}
+
+struct KeyboardPreviewCard_Previews: PreviewProvider {
+   private static let ctx = KeyboardContext.preview
+   static func input(for locale: KeyboardLocale) -> KeyboardInputSetProvider {
+       StandardKeyboardInputSetProvider(
+           context: ctx,
+           providers: [provider(for: locale)])
+   }
+   static func layout(for locale: KeyboardLocale) -> KeyboardLayoutProvider {
+       iPhoneKeyboardLayoutProvider(inputSetProvider: input(for: locale))
+   }
+   
+   static func provider(for locale: KeyboardLocale) -> LocalizedKeyboardInputSetProvider {
+       switch locale {
+//        case .swedish: return InternalSwedishKeyboardInputSetProvider(device: MockDevice())
+       default: return EnglishKeyboardInputSetProvider(device: FakeMockDevice())
+       }
+   }
+   
+   static var previews: some View {
+       Group {
+           // A standard system keyboard
+           SystemKeyboard(
+               layout: .preview,
+               appearance: PreviewKeyboardAppearance.preview,
+               actionHandler: PreviewKeyboardActionHandler.preview,
+               context: .preview,
+               inputContext: nil,
+               secondaryInputContext: nil,
+               width: UIScreen.main.bounds.width)
+       }
+   }
 }
--- Tests/MyKeyboardKitPreviewTests/MyKeyboardKitPreviewTests.swift
+++ Tests/MyKeyboardKitPreviewTests/MyKeyboardKitPreviewTests.swift
@@ -6,6 +6,6 @@ final class MyKeyboardKitPreviewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyKeyboardKitPreview().text, "Hello, World!")
+        //XCTAssertEqual(MyKeyboardKitPreview().text, "Hello, World!")
     }
 }
